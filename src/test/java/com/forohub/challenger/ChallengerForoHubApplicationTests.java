17cfad74009ed60b76edb6fc6910ab6e4eea363c
Alejandra Contreras <user.email>
2025-01-21 02:02:54 -0300

    Primer commit

---
+++ src/test/java/com/forohub/challenger/ChallengerForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forohub.challenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengerForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
